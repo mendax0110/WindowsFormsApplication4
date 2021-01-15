baae9c557da2d4123498d9db3415636d00508d56
Adrian Goessl <adrian.goessl@outlook.com>
2021-01-15 02:24:07 +0100

    Projektdateien hinzufügen.

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelmaxGeschwindigkeit = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelmaxGeschwindigkeit
+            // 
+            this.labelmaxGeschwindigkeit.AutoSize = true;
+            this.labelmaxGeschwindigkeit.Location = new System.Drawing.Point(9, 18);
+            this.labelmaxGeschwindigkeit.Name = "labelmaxGeschwindigkeit";
+            this.labelmaxGeschwindigkeit.Size = new System.Drawing.Size(110, 13);
+            this.labelmaxGeschwindigkeit.TabIndex = 0;
+            this.labelmaxGeschwindigkeit.Text = "max. Geschwindigkeit";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 51);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "km/h";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Einheitwechseln";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(122, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(43, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "m/s";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(50, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "km/h";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(272, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(420, 129);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "UP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(91, 127);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Down";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 462);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelmaxGeschwindigkeit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelmaxGeschwindigkeit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            kmh = true;
+            aktuellegesch = 0;
+            maxgesch = 0;
+        }
+        int maxgesch;
+        int aktuellegesch;
+        bool kmh;
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                groupBox1.Text = "km/h";
+                kmh = true;
+
+            }
+            else
+            {
+                groupBox1.Text = "m/s";
+                kmh = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int zw = 0;
+            try
+            {
+                zw = Convert.ToInt16(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("max Gschwindigkeit muss eine zahl sein");
+            }
+            if(zw > 0)
+            {
+                maxgesch = zw;
+            }else
+            {
+                MessageBox.Show("max Gschwindigkeit muss größer null sein");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(aktuellegesch < (maxgesch-10))
+            {
+                aktuellegesch += 10;
+            }
+            else
+            {
+                aktuellegesch = maxgesch;
+            }
+            pictureBox1.Invalidate();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            if (aktuellegesch > 10)
+            {
+                aktuellegesch -= 10;
+            }
+            else
+            {
+                aktuellegesch = 0;
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics dev = e.Graphics;
+            Font font = new Font("Arial", 10);
+            if (kmh)
+            {
+                dev.DrawString("" + aktuellegesch + "km/h", font, Brushes.Black, 10, 10);
+                dev.FillRectangle(Brushes.Black, 20, 20, aktuellegesch * 2, 40);
+                dev.FillRectangle(Brushes.Black, aktuellegesch*2 - 2 +20 , 20, 4, 50);
+            }
+            else
+            {
+                dev.DrawString("" +(double)(aktuellegesch / 3.6) + "m/s", font, Brushes.Black, 10, 10);
+                dev.FillRectangle(Brushes.Black, 20, 20,(int) ((aktuellegesch/3.6) * 4)+20, 40);
+                dev.FillRectangle(Brushes.Black, (int)((aktuellegesch / 3.6) * 4)-2,20,4,50);
+            }
+        }
+    }
+}
